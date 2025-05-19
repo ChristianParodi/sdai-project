d32925189d7e2b3ecd047f1a9b200934a0657d9d
Christian Parodi <S4951409@studenti.unige.it>
2025-05-19 16:44:27 +0200

    Setup project

--- src/main/java/myextension/IntegerList.java
+++ src/main/java/myextension/IntegerList.java
@@ -8,14 +8,11 @@ import org.nlogo.core.SyntaxJ;
 public class IntegerList implements Reporter {
     // take a string as input, report a string
     public Syntax getSyntax() {
-        var input = new int[] {Syntax.StringType()};
-        var output = Syntax.StringType();
-        return SyntaxJ.reporterSyntax(input, output);
+        return SyntaxJ.reporterSyntax(new int[] {Syntax.StringType()}, Syntax.StringType());
     }
 
     public Object report(Argument args[], Context context)
             throws ExtensionException {
-        String str = "Ciao";
-        return str;
+        return "Ciao";
     }
 }
